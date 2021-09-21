dd153da380122459a73e74653cbb38e091a990c5
Rany Albeg Wein <RanyAlbegWein@users.noreply.github.com>
2021-09-21 16:55:30 +0300

    Initial commit

---
+++ src/main/java/com/rany/coffeenameserver/CoffeeNameServerApplication.java
@@ -0,0 +1,13 @@
+package com.rany.coffeenameserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeNameServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeNameServerApplication.class, args);
+    }
+
+}
